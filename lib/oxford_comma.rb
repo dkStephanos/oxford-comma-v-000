cec855aaa37411142cb14685a373fbb14a5456f8
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-10-27 20:32:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,13 @@
 def oxford_comma(array)
   array.size == 1 ? (return array[0]) : false
   array.size == 2 ? (return array[0] + " and " + array[1]) : false
+
+  output = ""
+  counter = 0
+  array.each do |str|
+    output += str
+    counter + 2 == array.size ? (output += " and ") : (output += ", ")
+    counter += 0
+  end
+  output
 end
