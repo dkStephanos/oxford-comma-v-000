68d6ae8800619a6efe95e0ba6b8a8df1f8ab4247
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-10-27 20:28:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,4 @@
 def oxford_comma(array)
   array.size == 1 ? (return array[0]) : false
-  array.size == 2 ? (return array[0] + array[1]) : false
+  array.size == 2 ? (return array[0] + " and " + array[1]) : false
 end
